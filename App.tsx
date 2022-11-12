6c724a13824ba2a350ba593ef698d00b5c90920d
Buyle Stephanie (s125014) <stephanie.buyle@student.ap.be>
2022-11-12 23:52:40 +0100

    fetch is gelukt (steph)

--- App.tsx
+++ App.tsx
@@ -1,20 +1,79 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { CollectionProps } from './types';
+import axios, { AxiosResponse } from 'axios';
+
+
 
 export default function App() {
+
+  // axios api call 
+  /* 
+  const [collectionData, setCollectionData] = useState<CollectionProps[]>([]);
+  console.clear();
+  console.log('Collection data: ', collectionData);
+  const key: string = "RcVFbOJg"
+
+  useEffect(() => {
+    axios
+      .get<CollectionProps[]>(`https://www.rijksmuseum.nl/api/nl/collection?key=RcVFbOJg&involvedMaker=Rembrandt+van+Rijn`)
+      .then((response: AxiosResponse) => {
+        setCollectionData(response.data);
+      });
+  }, []);
+  */
+
+  // via fetch 
+
+  /* const [isLoading, setLoading] = useState(true);
+  const [collectionData, setCollectionData] = useState<CollectionProps[]>([]);
+  console.log(collectionData);
+
+
+  useEffect(() => {
+    fetch('https://www.rijksmuseum.nl/api/nl/collection?key=RcVFbOJg&involvedMaker=Rembrandt+van+Rijn')
+      .then((response) => response.json())
+      .then((json) : CollectionProps => setCollectionData(json))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+
+  */
+
+  const [collectionData, setCollectionData] = useState<CollectionProps>();
+  //console.clear();
+  console.log(collectionData);
+
+  const fetchData = async () => {
+    const response = await fetch("https://www.rijksmuseum.nl/api/nl/collection?key=RcVFbOJg&involvedMaker=Rembrandt+van+Rijn")
+    if (!response.ok) {
+      throw new Error("Data could not be fetched");
+    }
+    else {
+      return response.json();
+    }
+  }
+
+  useEffect(() => {
+    
+    fetchData()
+      .then((res) => {
+        setCollectionData(res)
+      })
+      .catch((e) => { console.log(e.message) })
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    
+    <View style={{ flex: 1, padding: 24 }}>
+      {collectionData?.artObjects.map((e,index) => { return <Text>{e.id} - {e.title}</Text>})}
+     
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
+
