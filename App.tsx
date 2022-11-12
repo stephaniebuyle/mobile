10f975c8deb14fd0c097de3da1317d3292abddb2
Marloes van der Zalm <marloes@MacBook-Pro-van-Marloes.local>
2022-11-12 23:22:13 +0100

    werkende fetch

--- App.tsx
+++ App.tsx
@@ -1,13 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+
+interface Response {
+  artObject: {
+    description: string
+  }
+}
 
 export default function App() {
+
+const [data, setData] = useState<Response>();
+
+useEffect(() => {
+  getData();
+}, []);
+
+const getData = async () => {
+    
+    let response = await fetch('https://www.rijksmuseum.nl/api/nl/collection/SK-C-5?key=RcVFbOJg', {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      }
+    })
+
+    let json = await response.json();
+    setData(json as Response);
+}
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{data?.artObject.description} </Text>
     </View>
   );
+  
 }
 
 const styles = StyleSheet.create({
