3a75478bc6a03200742e65d3f52a29e655155068
Marloes van der Zalm <marloesvdzalm@gmail.com>
2022-12-12 21:36:51 +0100

    opzet kalender

--- components/screens/AgendaScreen.tsx
+++ components/screens/AgendaScreen.tsx
@@ -1,10 +1,11 @@
-import { View, Text } from "react-native"
-import Constants from "expo-constants";
+import { View } from "react-native";
+import Agenda from "../Agenda";
+import Kalender from "../Calendar";
 
 const AgendaScreen = () => {
     return(
         <View>
-            <Text>Agenda</Text>
+            <Agenda />
         </View>
     )
 }
