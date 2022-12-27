1fd0b8d365560e825393b6fd4b53fb0bfe407280
Buyle Stephanie (s125014) <stephanie.buyle@student.ap.be>
2022-12-27 16:03:25 +0100

    styling homepage

--- components/screens/HomeScreen.tsx
+++ components/screens/HomeScreen.tsx
@@ -2,92 +2,89 @@ import * as React from "react"
 import { StyleSheet, View, FlatList, Image, Text, Pressable } from "react-native"
 import { CollectionProps } from "../../types";
 import Constants from "expo-constants";
-import MasonryList from '@react-native-seoul/masonry-list';
 import { useNavigation } from "@react-navigation/native";
 
 const HomeScreen = () => {
-    const [collectionData, setCollectionData] = React.useState<CollectionProps>();
-    console.clear();
-    console.log(collectionData);
-    const page = Math.floor(Math.random() * 200) + 1;
-  
-    const fetchData = async () => {
-      const response = await fetch("https://www.rijksmuseum.nl/api/nl/collection?key=RcVFbOJg&p="+page)
-      if (!response.ok) {
-        throw new Error("Data could not be fetched");
-      }
-      else {
-        return response.json();
-      }
+  const [collectionData, setCollectionData] = React.useState<CollectionProps>();
+
+  const page = Math.floor(Math.random() * 200) + 1;
+
+  const fetchData = async () => {
+    const response = await fetch("https://www.rijksmuseum.nl/api/nl/collection?key=RcVFbOJg&p=" + page)
+    if (!response.ok) {
+      throw new Error("Data could not be fetched");
     }
+    else {
+      return response.json();
+    }
+  }
 
-    const navigation: any = useNavigation();
-  
-    React.useEffect(() => {
-      fetchData()
-        .then((res) => {
-          setCollectionData(res)
-        })
-        .catch((e) => { console.log(e.message) })
-    }, []);
+  const navigation: any = useNavigation();
 
-    return(
-        <View style={{ flex: 1, padding: 24, paddingTop: Constants.statusBarHeight}}>
-          <Text style={styles.titelApp}>Hollands Glorie</Text>
-        <FlatList
-              data={collectionData?.artObjects}
-              keyExtractor={({ id }, index) => id}
-              renderItem={({ item }) => (
+  React.useEffect(() => {
+    fetchData()
+      .then((res) => {
+        setCollectionData(res)
+      })
+      .catch((e) => { console.log(e.message) })
+  }, []);
 
-                      <Pressable
-                        onPress={() => {
-                            navigation.navigate("Detail", {item: item})}
-                        }
-                    >
-                        <View style={styles.container}>
+  return (
+    <View style={styles.container}>
+      <Text style={styles.titleApp}>Hollands Glorie</Text>
+      <FlatList
+        data={collectionData?.artObjects}
+        keyExtractor={({ id }, index) => id}
+        renderItem={({ item }) => (
 
-                            <Image style={styles.img}
-                              
-                                source={{ uri: item.webImage?.url }}
-                            />
-                            <Text style={styles.legend}>{item.title}</Text>
-                        </View>
-                    </Pressable>
-              )}
-            />
-      </View>
-    )
+          <Pressable
+            onPress={() => {
+              navigation.navigate("Detail", { item: item })
+            }
+            }
+          >
+            <View style={styles.card}>
+
+              <Image style={styles.img}
+
+                source={{ uri: item.webImage?.url }}
+              />
+              <Text style={styles.legend}>{item.title}</Text>
+            </View>
+          </Pressable>
+        )}
+      />
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
-      position: "relative"
+    flex: 1, 
+    alignItems: 'center',
+    paddingTop: Constants.statusBarHeight
   },
-  img:{
-      
-      height: 400, 
-      width: 400,
-      borderWidth : 1,
-      borderColor : "gray",
-     
+  card: {
+    width: 300,
+    position: "relative", 
+    marginBottom: 5
   },
-  legend:{
-      position : "absolute",
-      bottom:5,
-      left:5,
-      backgroundColor: "rgba(0,0,0,0.7)",
-      color: "#ffffff",
-      padding: 2, 
+  img: {
+    height: 350,
+    width: '100%',
+    resizeMode: 'contain',
+    borderWidth: 1,
+    borderColor: "gray",
   },
-  box: {
-      width: 100,
-      height: 100,
-      backgroundColor: "red",
-      borderWidth: 1,
-      borderColor: "black",
-      borderRadius: 10
-  }, 
-  titelApp: {
+  legend: {
+    position: "absolute",
+    bottom: 5,
+    left: 5,
+    backgroundColor: "rgba(0,0,0,0.7)",
+    color: "#ffffff",
+    padding: 2,
+  },
+  titleApp: {
     fontWeight: "bold",
     fontSize: 33,
     textAlign: "center"
